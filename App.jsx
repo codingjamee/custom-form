2d4ad7707b7d68a775aa7c9968f5ac7e930d3694
허제인 <codingjamee@gmail.com>
2024-07-09 23:32:04 +0900

    App파일 추가

---
+++ App.jsx
@@ -0,0 +1,31 @@
+/**  @jsx createElement */
+import { createDOM, render, createElement, useState } from "./core/react.js";
+import CheckBoxInput from "./common/CheckBoxInput.jsx";
+import RadioInput from "./common/RadioInput.jsx";
+
+const App = () => {
+  console.log(useState);
+  const [value, setValue] = useState(1);
+  console.log(value);
+
+  const onClickNext = (e) => {
+    e.preventDefault();
+    console.log("다음 클릭됨");
+    setValue((prev) => {
+      console.log(prev);
+      return prev + 1;
+    });
+    console.log(value);
+  };
+
+  return (
+    <form onSubmit={onClickNext}>
+      <RadioInput />
+      <CheckBoxInput />
+      <div style={{width: '300px', height: '300px'}}>{value}</div>
+      <button type="submit">제출</button>
+    </form>
+  );
+};
+
+export default App;
